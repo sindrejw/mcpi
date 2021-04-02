4646174539bf2656ca1840d1dbed488b6a2e0076
sinwo612 <sinwo612@student.liu.se>
2021-04-02 12:06:54 +0200

    Finished vanilla Python simulation

--- pyvanilla.py
+++ pyvanilla.py
@@ -1,3 +1,5 @@
+from random import random
+
 def estimate_pi(iterations=100):
     """Estimate pi using MC-simulation.
 
@@ -5,4 +7,15 @@ def estimate_pi(iterations=100):
     interations -- number of iterations used (default 100)
     """
 
-    
+    circle_points = 0
+
+    for i in range(iterations):
+        x, y = random(), random()
+
+        if x*x + y*y <= 1:
+            circle_points += 1
+
+    return 4*circle_points/iterations
+
+if (__name__ == '__main__'):
+    estimate_pi()
